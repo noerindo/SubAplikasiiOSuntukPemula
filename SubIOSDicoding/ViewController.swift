c8f95c5c5f53135e0c562543f1660da5970a05d1
indahnurindo <indahnoerindo@gmail.com>
2021-06-16 13:53:49 +0700

    Initial Commit

---
+++ SubIOSDicoding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SubIOSDicoding
+//
+//  Created by Indah Nurindo on 16/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
